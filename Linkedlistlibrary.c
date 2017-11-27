166b662ce6c690f4c3af5c3b7a85326276f57c4d
starsoulF <yslim1203@naver.com>
2017-11-28 01:44:29 +0900

    branch 1

--- Linkedlistlibrary.c
+++ Linkedlistlibrary.c
@@ -1,57 +1,119 @@
 #include "linkedlist.h"
 
-LINK CreateNode(int input){
-	LINK Cur;
-	if((Cur = (LINK)malloc(sizeof(Node))) == NULL){
-		printf("fail to create a node");
+LINK createNode(int *value)
+{
+	LINK cur;
+	cur = (LINK)malloc(sizeof(NODE));
+	if (cur == NULL)
+	{
+		printf("Error!");
 		return NULL;
 	}
-	Cur->value = input;
-	Cur->next = NULL;
+	//   cur->value = (int*)malloc(sizeof(int) * 2);
+	cur->value = value;
+	cur->next = NULL;
+	cnt++;
+	return cur;
 }
 
-void DeleteOddNode(LINK head, int sizeofNode){
-	LINK Cur = head;
-	for(int i = 0; i < sizeofNode; i++){
-		if(i = 0){
-			head = head->Cur;
-			free(Cur);
-			Cur = head;
+LINK append(LINK cur, LINK head)
+{
+	LINK nextNODE = head;
+	if (head == NULL)
+	{
+		head = cur;
+		return head;
+	}
+
+	while (nextNODE->next != NULL)
+	{
+		nextNODE = nextNODE->next;
+	}
+	nextNODE->next = cur;
+
+	return head;
+}
+
+void reverseList(LINK head)
+{
+	LINK nextNODE = head;
+	while (nextNODE != NULL)
+	{
+		printf("%d\n", nextNODE->value);
+		nextNODE = nextNODE->next;
+	}
+}
+
+void printList(LINK head)
+{
+	LINK nextNODE = head;
+	for (int i = 0; i < cnt; i++)
+	{
+		for (int j = 0; j < cnt - i - 1; j++)
+			nextNODE = nextNODE->next;
+		if (nextNODE->value != 0)
+			printf("%d\n", nextNODE->value);
+		nextNODE = head;
+	}
+}
+
+void deleteList(LINK head)
+{
+	LINK nextNODE = head;
+	while (nextNODE != NULL)
+	{
+		if (cnt % 2 == 1)
+		{
+			for (int i = 0; i < cnt; i++)
+			{
+				if (i % 2 == 0)
+				{
+					nextNODE->value = 0;
+				}
+				nextNODE = nextNODE->next;
+			}
 		}
-		else if(i%2 == 1){
-			Link temp = Cur->next;
-			Cur->next = temp->next;
-			free(temp);
+		if (cnt % 2 == 0)
+		{
+			for (int i = 0; i < cnt; i++)
+			{
+				if (i % 2 == 1)
+				{
+					nextNODE->value = 0;
+				}
+				nextNODE = nextNODE->next;
+			}
 		}
+
 	}
+	head = nextNODE;
+	return head;
 }
 
-int LengthofNode(LINK head){
-	int length=1;
-	LINK Cur = head;
-	while((Cur = Cur->next) != NULL){
-		++length;
-	}
-	return length;
-}
-
-void PrintNode(LINK head){
-	LINK Cur = head;
-	while(1){
-		printf("%d",Cur->value);
-		if((Cur = Cur->next) != NULL){
-			exit(1);
+void middleList(LINK head)
+{
+	LINK nextNODE = head;
+	if (cnt % 2 == 1)
+	{
+		for (int i = 0; i<(int)(cnt / 2 + 0.5); i++)
+		{
+			nextNODE = nextNODE->next;
 		}
+		printf("%d\n", nextNODE->value);
 	}
-}
+	if (cnt % 2 == 0)
+	{
+		for (int i = 0; i<(int)(cnt / 2 + 0.5); i++)
+		{
+			nextNODE = nextNODE->next;
+		}
+		printf("%d\n", nextNODE->value);
+		nextNODE = head;
+		for (int i = 0; i<(int)(cnt / 2 + 0.5) - 1; i++)
+		{
+			nextNODE = nextNODE->next;
+		}
+		printf("%d\n", nextNODE->value);
+	}
 
-void PrintMiddleNode(LINK head, int lengthofNode){
-	LINK Cur = head;
-	for(int i = 0; i < lengthofNode/2; i++){
-		Cur = Cur->next;
-	}
-	if(lengthofNode%2 == 1)
-		printf("%d",Cur->value);
-	else
-		printf("%d %d", Cur->value, Cur->next->value);
 }
