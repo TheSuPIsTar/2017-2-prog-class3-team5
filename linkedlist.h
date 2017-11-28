fde40ce9542a92080fb12ccf7da51d431c463e31
Lee seung chan <dltmdcks702@naver.com>
2017-11-28 09:31:17 +0900

    add origin function

--- linkedlist.h
+++ linkedlist.h
@@ -11,3 +11,10 @@ struct linked_list //자기참조 구조체에 대한 정의다.
 typedef struct linked_list NODE; //struct linked_list를 NODE라 재정의한다.
 
 typedef NODE * LINK; //NODE 포인터를 링크로 재정의한다.
+
+LINK CreateNode(int input);
+void DeleteOddNode(LINK head, int sizeofNode);
+void ReverseNode(LINK head);
+int LengthofNode(LINK head);
+void PrintNode(LINK head);
+void PrintMiddleNode(LINK head, int lengthofNode);
