e7c73886642b64469eb19d44113fbd8a2b34059b
Lee seung chan <dltmdcks702@naver.com>
2017-12-02 18:11:40 +0900

    add Trivial Solution

--- Rotate/RotatingLibrary.c
+++ Rotate/RotatingLibrary.c
@@ -1,1 +1,15 @@
 // this is a c file to hold function to rotate sting.
+
+#include "Rotate.h"
+
+char *TrivialPricySolution(char input[], int d){
+	char temp;
+	int length = strlen(input);
+	for(int i = 0; i < d; i++){
+		temp = input[0];
+		for(int j = 0; j < length; j++)
+			input[j] = input[j+1];
+		input[length-1] = temp;
+	}
+	return input[];
+}
